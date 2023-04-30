fe9677bb13d114f1c9abdeab327d5032b2bf2e15
wcc1101 <davidc1101@gmail.com>
2023-04-30 22:02:51 +0800

    hw3 v2

--- hw3/3.py
+++ hw3/3.py
@@ -43,20 +43,51 @@ def fullSearch(tar, ref, rowIndex, colIndex, searchRange):
     return np.array(best)
 
 def logSearch(tar, ref, rowIndex, colIndex, searchRange):
+    # step 1. initialize
+    center = (0, 0)
     best = None
     minSAD = np.inf
     step = searchRange // 2
-    while step > 0:
-        for i in range(-searchRange, searchRange + 1, step):
-            for j in range(-searchRange, searchRange + 1, step):
-                if rowIndex + i >= 0 and rowIndex + i < ref.shape[0] and colIndex + j >= 0 and colIndex + j < ref.shape[1]:
-                    nowSAD = calSAD(ref[rowIndex + i, colIndex + j], tar[rowIndex, colIndex])
-                    if nowSAD < minSAD:
-                        minSAD = nowSAD
-                        best = (i, j)
-        searchRange = step
+    M = []
+    updateM = True
+
+    while step >= 1:
+        # step 2. M(n)
+        if updateM:
+            M = [(0, 0), (step, 0), (0, step), (-step, 0), (0, -step)]
+            updateM = False
+
+        # step 3. find minimum
+        for (i, j) in M:
+            if 0 <= rowIndex + center[0] + i < ref.shape[0] and 0 <= colIndex + center[1] + j < ref.shape[1]:
+                nowSAD = calSAD(ref[rowIndex + center[0] + i, colIndex + center[1] + j], tar[rowIndex, colIndex])
+                if nowSAD < minSAD:
+                    minSAD = nowSAD
+                    best = (i, j)
+
+        # step 4. q <- q + i, l <- l + j, M <- M - (-i, -j), go to step 3
+        if best[0] != 0 or best[1] != 0:
+            print(best[0], best[1])
+            center = (center[0] + best[0], center[1] + best[1])
+            M = [(m[0] + best[0], m[1] + best[1]) for m in M]
+            continue
+
+        # step 5. n <- n / 2
         step //= 2
-    return np.array(best)
+        updateM = True
+        print('---------------- step size: ', step)
+
+    # step 6. find minimum in N(1)
+    for i in [-step, 0, step]:
+        for j in [-step, 0, step]:
+            if 0 <= rowIndex + center[0] + i < ref.shape[0] and 0 <= colIndex + center[1] + j < ref.shape[1]:
+                nowSAD = calSAD(ref[rowIndex + center[0] + i, colIndex + center[1] + j], tar[rowIndex, colIndex])
+                if nowSAD < minSAD:
+                    minSAD = nowSAD
+                    best = (i, j)
+    center = (center[0] + best[0], center[1] + best[1])
+
+    return np.array(center)
 
 def calMotionVector(ref, tar, searchRange, searchMethod):
     # shape of macroblocks
@@ -89,20 +120,31 @@ def PSNR(image1, image2):
 
 if __name__ == '__main__':
     # get input
-    imageRef = cv2.imread('img/40.jpg')
-    imageTar = cv2.imread('img/42.jpg')
+    image40 = cv2.imread('img/40.jpg')
+    image42 = cv2.imread('img/42.jpg')
+    image51 = cv2.imread('img/51.jpg')
     # convert BGR to RGB
-    imageRef = imageRef[:,:,::-1]
-    imageTar = imageTar[:,:,::-1]
+    image40 = image40[:,:,::-1]
+    image42 = image42[:,:,::-1]
+    image51 = image51[:,:,::-1]
 
-    d = {'full': fullSearch, '2d': logSearch}
-
+    # part 1
+    # d = {'full': fullSearch, '2d': logSearch}
+    d = {'2d': logSearch}
     for blockSize in [8, 16]:
-        mbRef = macroblockDivide(imageRef, blockSize)
-        mbTar = macroblockDivide(imageTar, blockSize)
+        mbRef = macroblockDivide(image40, blockSize)
+        mbTar = macroblockDivide(image42, blockSize)
         for searchMethod in d.keys():
             for searchRange in [8, 16]:
                 mv = calMotionVector(mbRef, mbTar, searchRange, d[searchMethod])
-                imagePdt = predict(imageRef, mv, blockSize)
+                imagePdt = predict(image40, mv, blockSize)
                 saveImage(imagePdt[:,:,::-1], f'{searchMethod}_predicted_r{searchRange}_b{blockSize}.jpg')
-                print('{}_r{}_b{} -- SAD: {}, PSNR: {:2.4f}'.format(searchMethod, searchRange, blockSize, calSAD(imageTar, imagePdt), PSNR(imageTar, imagePdt)))
+                print('{}_r{}_b{} -- SAD: {}, PSNR: {:2.4f}'.format(searchMethod, searchRange, blockSize, calSAD(image42, imagePdt), PSNR(image42, imagePdt)))
+
+    # part 2
+    mbRef = macroblockDivide(image40, 8)
+    mbTar = macroblockDivide(image51, 8)
+    mv = calMotionVector(mbRef, mbTar, 8, logSearch)
+    imagePdt = predict(image40, mv, 8)
+    saveImage(imagePdt[:,:,::-1], f'40to51.jpg')
+    print('40 to 51 -- SAD: {}, PSNR: {:2.4f}'.format(calSAD(image51, imagePdt), PSNR(image51, imagePdt)))
